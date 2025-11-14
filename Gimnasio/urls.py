94de1ca8c2af8fe2d13b2f1badbbf76b83594742
JoquinH <juakin2013torres@gmail.com>
2025-11-14 12:59:21 -0300

    agregre exito en urls

--- Gimnasio/urls.py
+++ Gimnasio/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from Clientes import views
-
+#exito
 urlpatterns = [
     # Admin de Django (NO TOCAR)
     path('admin/', admin.site.urls),
